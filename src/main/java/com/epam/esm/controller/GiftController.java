491e5c927692df9c000bc7e39ba10c851a61fb0d
RGReeTy <be.a.reety@gmail.com>
2020-10-27 23:23:35 +0300

    add controller

---
+++ src/main/java/com/epam/esm/controller/GiftController.java
@@ -0,0 +1,9 @@
+package com.epam.esm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class GiftController {
+}
