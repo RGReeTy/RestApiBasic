e5fff6a705ed14a8676b0db5a89a03e88748e455
RGReeTy <be.a.reety@gmail.com>
2020-10-31 12:44:07 +0300

    update model

--- src/main/java/com/epam/esm/model/GiftCertificate.java
+++ src/main/java/com/epam/esm/model/GiftCertificate.java
@@ -1,21 +1,28 @@
 package com.epam.esm.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class GiftCertificate {
 
+    private long id;
     private String name;
     private String description;
     private BigDecimal price;
-    private LocalDate createDate;
-    private LocalDate lastUpdateDate;
+    private ZonedDateTime createDate;
+    private ZonedDateTime lastUpdateDate;
     //Duration - in days (expiration period)
-    private int duration;
+    private Integer duration;
+    private List<Tag> tagList;
 
 }
--- src/main/java/com/epam/esm/model/Tag.java
+++ src/main/java/com/epam/esm/model/Tag.java
@@ -2,10 +2,13 @@ package com.epam.esm.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Tag {
 
+    private long id;
     private String name;
 }
